d754d5337e24ae4f1a64072472fdc357160f5ec9
Chrismung <142159288+Chrismung@users.noreply.github.com>
2024-10-16 22:36:20 +1100

    ui done

---
+++ Assets/Scripts/UIButtons.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIButtons : MonoBehaviour
+{
+    public void LoadFirstLevel()
+    {
+        
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
