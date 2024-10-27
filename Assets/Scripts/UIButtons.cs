a7b6b41914b0daabd38ea48f956fa3abf4373279
Chrismung <142159288+Chrismung@users.noreply.github.com>
2024-10-27 23:09:09 +1100

    ingameui done

--- Assets/Scripts/UIButtons.cs
+++ Assets/Scripts/UIButtons.cs
@@ -11,6 +11,12 @@ public class UIButtons : MonoBehaviour
         SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
     }
 
+    public void LoadExit()
+    {
+
+        SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
